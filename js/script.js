8a205103016956d4457d81a88bc9ab64672849d4
Anna Agozzino <anna.agozzino91@gmail.com>
2020-08-26 15:09:47 +0200

    Inizio progetto

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//Scrivi un programma che stampi i numeri da 1 a 100, ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz. Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+//Stampare i numeri da 1 a 100
+
+// Se numero % 3 == 0 stampo Fizz
+
+// Se numero % 5 == 0 stampo FizzBuzz
+
+// Se numero % 3 == 0 && numero % 5 == 0 stampo FizzBuzz
